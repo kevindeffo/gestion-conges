11c846fea80d961a84f33909dab41aec67c1536e
Elliot <elliot@codeaura.ai>
2024-10-02 00:08:51 +0100

    Ajout du formulaire de connexion à la vue 'login.php'.

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,5 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-
-
+$routes->get('login', 'Auth::index');
+$routes->post('login', 'Auth::login');
+$routes->get('logout', 'Auth::logout');
+$routes->get('forgot-password', 'Auth::forgotPassword');
