456d3a729b20561404b94f7115d04d5a12b8405c
kevindeffo <deffokevin14@gmail.com>
2024-10-09 22:57:42 +0100

    update layout

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/tables', function () {
+    return view('tables');
+})->middleware(['auth']);
+
 require __DIR__.'/auth.php';
