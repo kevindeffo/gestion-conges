8a8615010bac90598ca684a0d2052a2805a8cc7d
kevindeffo <deffokevin14@gmail.com>
2024-10-09 06:50:02 +0100

    change from code igniter to laravel

--- app/Views/layout/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="text-center py-6">
-  <p class="mb-0 fs-4">Design and Developed by 
-    <a href="https://www.wrappixel.com/" target="_blank" class="pe-1 text-primary text-decoration-underline">Wrappixel.com</a>
-  </p>
-</footer>
